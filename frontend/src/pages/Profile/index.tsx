2e9b97ac9fb198a2dba84d4874948bc7a7696557
Bruno Felix Dias <brunofelix.ml@gmail.com>
2020-08-26 21:31:42 -0300

    feat: Adding the signOut method in useAuth and using it in the page profile

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+import { useAuth } from '../../hooks/auth'
 
 function Profile(): JSX.Element {
+    const { signOut } = useAuth()
+    let history = useHistory()
+
+    const handleSignOut = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        event.preventDefault()
+        signOut()
+        history.push("/signin")
+    }
+
     return (
         <>
             <div>
+                <button style={{ marginTop: "200px" }}
+                    onClick={handleSignOut} >Sign out</button>
                 <h1 style={{ marginTop: "200px" }}>Profile</h1>
             </div>
         </>
